f425dfe0b8b04eebf3886e2b9ee1d5ab01604f17
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2024-11-16 15:13:31 +0100

    Darkmode.jsx
    n

--- src/components/Navbar/Darkmode.jsx
+++ src/components/Navbar/Darkmode.jsx
@@ -7,7 +7,7 @@ const Darkmode = ({darkMode, toggleMode}) => {
   return (
     <div className='relative'>
       <button onClick={toggleMode}>
-        {darkMode ? <img src={Lightpng} alt="Lightmode" className='w-14 h-8 drop-shadow-xl cursor-pointer lg:w-20 lg:h-12 transition-all duration-300'/> :  <img src={Darkpng} alt="Lightmode" className='w-14 h-8 drop-shadow-xl cursor-pointer lg:w-20 lg:h-12 transition-all duration-300 ' />}
+        {darkMode ? <img src={Lightpng} alt="Lightmode" className='w-10 h-8 drop-shadow-xl cursor-pointer lg:w-20 lg:h-12 transition-all duration-300'/> :  <img src={Darkpng} alt="Lightmode" className='w-10 h-8 drop-shadow-xl cursor-pointer lg:w-20 lg:h-12 transition-all duration-300 ' />}
       </button>
     </div>
   )
