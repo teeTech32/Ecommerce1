20de4922cb42ff35431b6d9f9eb4c081927a3c11
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2024-11-16 14:58:18 +0100

    Navbar.jsx
    a

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({darkMode, toggleMode, handlePopup}) => {
     <div className="shadow-md">
       {/*upper navbar*/}
       <div className='bg-primary/40 py-2 lg:py-4 dark:bg-gray-900 dark:text-white shadow-xl  duration-200 relative z-40'>
-        <div className='w-full flex-col py-5 pl-4 pr-6 lg:container lg:flex justify-between lg:items-center lg:flex-row'>
+        <div className='w-full flex-col py-5 pl-2 pr-6 lg:container lg:flex justify-between lg:items-center lg:flex-row'>
           <div className='mb-5' >
             <a href="/#" onClick={()=>{ toast.success("Welcome Home")}} className='font-bold lg:text-4xl sm:text-2xl flex gap-x-2 lg:hover:scale-125 hover:scale-75 ease-in-out duration-500'>
               <img src={logo}  alt="Logo" className='w-10 lg:w-14'/>
@@ -24,7 +24,7 @@ const Navbar = ({darkMode, toggleMode, handlePopup}) => {
           {/*search bar and other buttons */}
           <div className='flex gap-4 md:gap-4 lg:gap-6 items-center'>
             <div className='group relative '>
-              <input type="text" placeholder='search' className=' sm:w-[150px] lg:w-[300px] group-hover:w-[250px] lg:group-hover:w-[400px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 lg:px-4 lg:py-2 focus:outline-none focus:border-1 focus:border-primary dark:text-black' />
+              <input type="text" placeholder='search' className=' sm:w-[100px] lg:w-[300px] group-hover:w-[250px] lg:group-hover:w-[400px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 lg:px-4 lg:py-2 focus:outline-none focus:border-1 focus:border-primary dark:text-black' />
                <IoMdSearch  className='text-3xl lg:text-4xl absolute text-black bottom-4 right-2 translate-y-1/2'/>
             </div>
               <button onClick={()=>handlePopup()} className="bg-gradient-to-r from-primary to-secondary text-white rounded-full py-1 px-6  transition-all duration-200 group gap-3 flex items-center ">
